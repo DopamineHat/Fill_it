327354f6f7737feda81eeac434f8078f3b98e1a6
UglyHat <rpagot@hotmail.fr>
2017-02-25 03:10:39 +0100

    yolo

--- WIP/place.c
+++ WIP/place.c
@@ -6,10 +6,23 @@
 /*   By: rpagot <rpagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 22:40:04 by rpagot            #+#    #+#             */
-/*   Updated: 2017/02/24 22:42:10 by rpagot           ###   ########.fr       */
+/*   Updated: 2017/02/25 03:10:15 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fill_it.h"
 
-void	ft_placetetri(
+unsigned short	*ft_placetetri(unsigned short *prepos, unsigned short tetri)
+{
+	int x;
+
+	x = 0;
+	while (x < 256)
+	{
+		if ((*prepos ^ tetri) == (*prepos | tetri) && ((*prepos << (x % 16)
+					>> (15 - (x % 16))) == 1))
+			ft_verifytetri(
+	*prepos++;
+	++x;
+	}
+
