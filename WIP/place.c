09f739f016fbb6e278c685168000ec298b368c41
UglyHat <rpagot@hotmail.fr>
2017-02-26 09:27:56 +0100

    ihopeitworks

--- WIP/place.c
+++ WIP/place.c
@@ -6,31 +6,34 @@
 /*   By: rpagot <rpagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 22:40:04 by rpagot            #+#    #+#             */
-/*   Updated: 2017/02/26 04:10:44 by rpagot           ###   ########.fr       */
+/*   Updated: 2017/02/26 09:17:11 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fill_it.h"
 
-unsigned short	*ft_placetetri(unsigned short *prepos, unsigned short *tetri)
+unsigned short	ft_placetetri(unsigned short *prepos, unsigned short *tetri)
 {
 	int x;
-	int tmp;
 
 	x = 0;
-	tmp = 0;
 	while (x < 256)
 	{
-		tmp = x;
-		while (tmp < 16)
-		{
-			tmp -= 16;
-			tetri++;
-		}
 		if ((*prepos & *tetri) == 0)
-			ft_verifytetri(*prepos, x);
-		prepos++;
-		++x;
+		{
+			*prepos = *prepos & *tetri;
+			x = (x / 16) * 16;
+		}
+		else
+		{
+			*tetri = *tetri >> 1;
+			++x;
+		}
+		if (x % 16 == 0)
+		{
+			tetri++;
+			prepos++;
+		}
 	}
 }
 
