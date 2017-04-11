97b24af8d57b77bf789918c71136ecc2cfd59731
Roman LEMASSON <rolemass@e2r13p10.42.fr>
2017-04-11 23:42:04 +0200

    up protection malloc

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rolemass <rolemass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 09:26:15 by rpagot            #+#    #+#             */
-/*   Updated: 2017/04/11 23:15:28 by rpagot           ###   ########.fr       */
+/*   Updated: 2017/04/11 23:41:36 by rolemass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int		main(int argc, char **argv)
 {
 	t_tetri tetri;
 
-	if (!((tetri.tetri = (unsigned short *)malloc(32 * sizeof(unsigned short)))
-			|| (tetri.tetriception = (unsigned short *)malloc(4
+	if (!(tetri.tetri = (unsigned short *)malloc(32 * sizeof(unsigned short)))
+			|| !(tetri.tetriception = (unsigned short *)malloc(4
 			* sizeof(unsigned short)))
-			|| (tetri.map = (unsigned short *)malloc(16
-			* sizeof(unsigned short)))))
+			|| !(tetri.map = (unsigned short *)malloc(16
+			* sizeof(unsigned short))))
 		exit(EXIT_FAILURE);
 	tetri.x = 0;
 	tetri.map = NULL;
@@ -32,7 +32,7 @@ int		main(int argc, char **argv)
 	}
 	if (ft_read_fd(open(argv[1], O_RDONLY), tetri) == -1)
 	{
-		ft_putendl_fd("error", 2);
+		ft_putendl_fd("error?", 2);
 		return (1);
 	}
 	return (0);
