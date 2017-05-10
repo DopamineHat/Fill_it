6927000a41a83c9c9360caab4e597681a252902a
UglyHat <rpagot@hotmail.fr>
2017-05-10 14:46:16 +0200

    up

--- includes/fill_it.h
+++ includes/fill_it.h
@@ -6,7 +6,7 @@
 /*   By: rpagot <rpagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 16:49:39 by rpagot            #+#    #+#             */
-/*   Updated: 2017/05/10 14:27:13 by rolemass         ###   ########.fr       */
+/*   Updated: 2017/05/10 14:41:27 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define FILL_IT_H
 
 # include "../Libft/includes/libft.h"
-#include <stdio.h> //WARNING
 
 # define BUF_SIZE 21
 
