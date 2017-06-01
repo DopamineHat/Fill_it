4c091f3206628b492619f36c7e30974aac7a4e8d
UglyHat <rpagot@hotmail.fr>
2017-06-01 08:00:46 +0200

    heheXD

--- Libft/srcs/ft_memchr.c
+++ Libft/srcs/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rpagot <rpagot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 21:35:50 by rpagot            #+#    #+#             */
-/*   Updated: 2016/11/18 21:35:52 by rpagot           ###   ########.fr       */
+/*   Updated: 2017/06/01 07:59:42 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((void *)s);
+			return ((void *)s + i);
 		++i;
 	}
 	return (NULL);
